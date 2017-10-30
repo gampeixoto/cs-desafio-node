70cf2fe0f82d8572e6c630c46de8cead62f8aa79
Gabriel Peixoto <gabriel.a.m.peixoto@hotmail.com>
2017-10-30 09:01:29 -0300

    Remove SSL key

--- server.js
+++ server.js
@@ -6,7 +6,5 @@ const http = require('http');
 var app = require('./config/express')();
 require('./config/database.js')(process.env.MONGODB||'mongodb://localhost/cs-desafio-node');
 
-let privateKey = fs.readFileSync('ssl.key').toString();
-let certificate = fs.readFileSync('ssl.crt').toString();
 
-http.createServer(option, app).listen(app.get('port'), ()=>(console.log(app.get('port'))));
+http.createServer(app).listen(app.get('port'), ()=>(console.log(app.get('port'))));
